05eea9d2d112a71efb89b8e720484391ee376410
manemone <hamada.kazuki.mum@gmail.com>
2015-07-22 05:15:26 +0900

    "post"

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api, format: :json do
     namespace :v1 do
-      resources :comments, only: [:index]
+      resources :comments, only: [:index, :create]
     end
   end
 end
