3084faf8a4eccb05da5641ded2343eed9dc0a329
manemone <hamada.kazuki.mum@gmail.com>
2015-07-22 18:51:58 +0900

    "Adding new comments"

--- app/assets/javascripts/components/comment_box.js.jsx
+++ app/assets/javascripts/components/comment_box.js.jsx
@@ -12,6 +12,21 @@ var CommentBox = React.createClass({
     })
   },
 
+  handleCommentSubmit: function(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({ data: this.state.data.concat([data]) });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+
   getInitialState: function() {
     return { data: [] }
   },
@@ -26,7 +41,7 @@ var CommentBox = React.createClass({
         <div className="commentBox">
           <h1>Comments</h1>
           <CommentList data={ this.state.data } />
-          <CommentForm />
+          <CommentForm onCommentSubmit={ this.handleCommentSubmit } />
         </div>
     );
   }
@@ -72,6 +87,36 @@ var Comment = React.createClass({
           <h2 className="commentAuthor">{ this.props.author }</h2>
           <span dangerouslySetInnerHTML={{ __html: rawMarkup }} />
         </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({ author: author, text: text });
+
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+
+    return;
+  },
+
+  render: function() {
+    return (
+      <form className="CommentForm" onSubmit={ this.handleSubmit }>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something..." ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
